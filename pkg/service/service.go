b5f918d6219957a03005d522ee56b8ccb9befb42
Danil <danilzign0909@gmail.com>
2023-12-16 22:34:59 +0300

    firts commit

---
+++ pkg/service/service.go
@@ -0,0 +1,42 @@
+package service
+
+import (
+	"github.com/danilzign/todo-app"
+	"github.com/danilzign/todo-app/pkg/repository"
+)
+
+type Authorization interface {
+	CreateUser(user todo.User) (int, error)
+	GenerateToken(username, password string) (string, error)
+	ParseToken(token string) (int, error)
+}
+
+type TodoList interface {
+	Create(userId int, list todo.TodoList) (int, error)
+	GetAll(userId int) ([]todo.TodoList, error)
+	GetById(userId, listId int) (todo.TodoList, error)
+	DeleteList(userId, listId int) error
+	UpdateList(userId, listId int, input todo.UpdateListInput) error
+}
+
+type TodoItem interface {
+	Create(iserId, listId int, item todo.TodoItem) (int, error)
+	GetAll(userId, listId int) ([]todo.TodoItem, error)
+	GetById(userId, itemId int) (todo.TodoItem, error)
+	Delete(userId, itemId int) error
+	Update(userId, itemId int, input todo.UpdateItemInput) error
+}
+
+type Service struct {
+	Authorization
+	TodoList
+	TodoItem
+}
+
+func NewService(repos *repository.Repository) *Service {
+	return &Service{
+		Authorization: NewAuthService(repos.Authorization),
+		TodoList:      NewTodoListService(repos.TodoList),
+		TodoItem:      NewTodoItemService(repos.TodoItem, repos.TodoList),
+	}
+}
---
+++ pkg/service/todo_list.go
@@ -0,0 +1,40 @@
+package service
+
+import (
+	"github.com/danilzign/todo-app"
+	"github.com/danilzign/todo-app/pkg/repository"
+)
+
+type TodoListService struct {
+	repo repository.TodoList
+}
+
+func NewTodoListService(repo repository.TodoList) *TodoListService {
+	return &TodoListService{repo: repo}
+}
+
+func (s *TodoListService) Create(userId int, list todo.TodoList) (int, error) {
+	return s.repo.Create(userId, list)
+}
+
+func (s *TodoListService) GetAll(userId int) ([]todo.TodoList, error) {
+	return s.repo.GetAll(userId)
+
+}
+
+func (s *TodoListService) GetById(userId, listId int) (todo.TodoList, error) {
+	return s.repo.GetById(userId, listId)
+
+}
+
+func (s *TodoListService) DeleteList(userId, listId int) error {
+	return s.repo.DeleteList(userId, listId)
+
+}
+
+func (s *TodoListService) UpdateList(userId, listId int, input todo.UpdateListInput) error {
+	if err := input.Validate(); err != nil {
+		return err
+	}
+	return s.repo.UpdateList(userId, listId, input)
+}
